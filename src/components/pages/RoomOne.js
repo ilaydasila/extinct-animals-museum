7b1d6ab6e5ffb5d901152de60e088a1e0e2ec2fd
ilaydasila <sila.kucukoz@gmail.com>
2020-06-24 11:35:53 +0200

    other animal maps are added with lorem ipsum text

--- src/components/pages/RoomOne.js
+++ src/components/pages/RoomOne.js
@@ -13,24 +13,27 @@ const RoomOne = (props) => {
       className: "passengerPigeon",
       name: "Passenger Pigeon",
       img: "/assets/passengerPigeon.png",
-      details: "Dead",
-      map: "",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis suscipit est, nec fermentum justo. Sed malesuada neque at nulla tempus, condimentum porta risus rutrum. Cras risus sapien, posuere ac tincidunt tincidunt, euismod nec eros. Integer id ipsum a justo ornare tristique sed ut lectus. Nulla suscipit purus in turpis maximus, vitae posuere odio consectetur. Quisque interdum vel erat ut vulputate. Aenean arcu elit, ultrices nec mattis non, aliquet id felis. Ut vestibulum, urna eget finibus ornare, dui arcu hendrerit urna, sed molestie lorem purus in tortor. Aliquam quis hendrerit lacus. Sed libero odio, elementum a nisi nec, commodo tempor ipsum. Aenean non mauris auctor, volutpat nibh vitae, gravida augue. Etiam mollis malesuada quam vel lacinia. Suspendisse tempus enim a libero tempus, quis fringilla enim fringilla. Suspendisse ex dolor, tempor sit amet enim in, congue gravida orci. Fusce tempor elit in dui consectetur, et volutpat ex ullamcorper.",
+      map: "/assets/passengerPigeonMap.png",
     },
     {
       id: 2,
       className: "seaMink",
       name: "Sea Mink",
       img: "/assets/seaMink.png",
-      details: "Dead",
-      map: "",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis suscipit est, nec fermentum justo. Sed malesuada neque at nulla tempus, condimentum porta risus rutrum. Cras risus sapien, posuere ac tincidunt tincidunt, euismod nec eros. Integer id ipsum a justo ornare tristique sed ut lectus. Nulla suscipit purus in turpis maximus, vitae posuere odio consectetur. Quisque interdum vel erat ut vulputate. Aenean arcu elit, ultrices nec mattis non, aliquet id felis. Ut vestibulum, urna eget finibus ornare, dui arcu hendrerit urna, sed molestie lorem purus in tortor. Aliquam quis hendrerit lacus. Sed libero odio, elementum a nisi nec, commodo tempor ipsum. Aenean non mauris auctor, volutpat nibh vitae, gravida augue. Etiam mollis malesuada quam vel lacinia. Suspendisse tempus enim a libero tempus, quis fringilla enim fringilla. Suspendisse ex dolor, tempor sit amet enim in, congue gravida orci. Fusce tempor elit in dui consectetur, et volutpat ex ullamcorper.",
+      map: "/assets/seaMinkMap.png",
     },
     {
       id: 3,
       className: "tecopaPupfish",
       name: "Tecopa Pupfish",
       img: "/assets/tecopaPupfish.png",
-      details: "Dead",
-      map: "",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis suscipit est, nec fermentum justo. Sed malesuada neque at nulla tempus, condimentum porta risus rutrum. Cras risus sapien, posuere ac tincidunt tincidunt, euismod nec eros. Integer id ipsum a justo ornare tristique sed ut lectus. Nulla suscipit purus in turpis maximus, vitae posuere odio consectetur. Quisque interdum vel erat ut vulputate. Aenean arcu elit, ultrices nec mattis non, aliquet id felis. Ut vestibulum, urna eget finibus ornare, dui arcu hendrerit urna, sed molestie lorem purus in tortor. Aliquam quis hendrerit lacus. Sed libero odio, elementum a nisi nec, commodo tempor ipsum. Aenean non mauris auctor, volutpat nibh vitae, gravida augue. Etiam mollis malesuada quam vel lacinia. Suspendisse tempus enim a libero tempus, quis fringilla enim fringilla. Suspendisse ex dolor, tempor sit amet enim in, congue gravida orci. Fusce tempor elit in dui consectetur, et volutpat ex ullamcorper.",
+      map: "/assets/tecopaPupfishMap.png",
     },
     {
       id: 4,
