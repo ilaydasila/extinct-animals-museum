3ccd3f2a2be551c3084e8f7ed03f0fa1cbf777ea
ilaydasila <sila.kucukoz@gmail.com>
2020-06-26 14:28:17 +0200

    fence seperated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import RoomOne from "./components/pages/RoomOne";
+import Image from "./components/Image";
 
 function App() {
   return (
     <div className='App'>
       <span>EXTINCT ANIMALS MUSEUM</span>
       <RoomOne></RoomOne>
+      <Image className='fence' alt='fence' img='./assets/fence.png'></Image>
     </div>
   );
 }
