061b24e4a03fdbfb6b57f455c353a04a1f2075b1
ilaydasila <sila.kucukoz@gmail.com>
2020-06-30 09:54:09 +0200

    tasmanian Tiger added

--- src/components/pages/RoomOne.js
+++ src/components/pages/RoomOne.js
@@ -40,6 +40,16 @@ const RoomOne = (props) => {
     },
     {
       id: 4,
+      className: "tasmanianTiger",
+      name: "Tasmanian Tiger",
+      img: "/assets/tasmanianTiger.png",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis suscipit est, nec fermentum justo. Sed malesuada neque at nulla tempus, condimentum porta risus rutrum. Cras risus sapien, posuere ac tincidunt tincidunt, euismod nec eros. Integer id ipsum a justo ornare tristique sed ut lectus. Nulla suscipit purus in turpis maximus, vitae posuere odio consectetur. Quisque interdum vel erat ut vulputate. Aenean arcu elit, ultrices nec mattis non, aliquet id felis. Ut vestibulum, urna eget finibus ornare, dui arcu hendrerit urna, sed molestie lorem purus in tortor. Aliquam quis hendrerit lacus. Sed libero odio, elementum a nisi nec, commodo tempor ipsum. Aenean non mauris auctor, volutpat nibh vitae, gravida augue. Etiam mollis malesuada quam vel lacinia. Suspendisse tempus enim a libero tempus, quis fringilla enim fringilla. Suspendisse ex dolor, tempor sit amet enim in, congue gravida orci. Fusce tempor elit in dui consectetur, et volutpat ex ullamcorper.",
+      map: "/assets/tasmanianTigerMap.png",
+      reasons: ["huntingPoaching", "habitatDestruction"],
+    },
+    {
+      id: 5,
       className: "dodo",
       name: "Dodo",
       img: "/assets/dodo.png",
